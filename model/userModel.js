5748852429ba3a7d1c738c096ebc274a55ccdbf3
usama9293 <usamaarshad9293@gmail.com>
2024-08-15 12:37:45 +0500

    "user model created"

---
+++ model/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  balance: {
+    type: Number,
+    default: 0,
+    required: [true, "balance is required"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
