9c50ef9b6e5c4ae6d7d37753cb8f14e62dd29816
usama9293 <usamaarshad9293@gmail.com>
2024-08-29 16:19:09 +0500

    feat: Add recent transactions to user dashboard

--- modules/user/controllers/userDashboard.js
+++ modules/user/controllers/userDashboard.js
@@ -1,4 +1,5 @@
 import User from "../../../model/userModel.js";
+import Transaction from "../../../model/transaModel.js";
 
 const userDashboard = async (req, res) => {
   const userId = req.user.id;
@@ -6,6 +7,10 @@ const userDashboard = async (req, res) => {
   try {
     // Find the user by ID and exclude the password field
     const user = await User.findById(userId).select("-password").exec();
+    const transactions = await Transaction.find({ user_id: userId })
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .exec();
 
     if (!user) {
       return res.status(404).json({
@@ -18,6 +23,7 @@ const userDashboard = async (req, res) => {
       message: "User details",
       status: "success",
       data: user,
+      transactions: transactions,
     });
   } catch (err) {
     return res.status(500).json({
